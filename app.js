de94d76374dfc818a4b5acb46ddbb14f0ef6fe18
kacram21 <acrameinstein@gmail.com>
2023-09-08 10:47:36 +0300

    how to use express

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
-app.use('/', (req, res) => {console.log('Running')});
 
+app.use( (req, res,next) => {
+    console.log('Running');
+    next();
+});
 
+app.use( (req, res,next) => {
+        res.send('<h1>The return of eng paul to web development<h1/>');});
 app.listen(3000);
