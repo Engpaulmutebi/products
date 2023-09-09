dab534211a8d9bb19a05a82e140aab41f33cddc7
kacram21 <acrameinstein@gmail.com>
2023-09-10 00:13:11 +0300

    adding the page not found

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const adminRouter = require('./routes/Admin');
 const shopRouter = require('./routes/shop');
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended:false }));
 app.use(adminRouter);
 app.use(shopRouter);
-app.use(bodyParser.urlencoded({ extended:false })); 
+
+app.use((req, res,next) =>{res.status(404).send('<h1>Page not found</h1>')});
+ 
 
 app.listen(3000);
