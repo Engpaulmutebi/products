76e082eddb712ec3d1d018d8667de9da70dbf69a
kacram21 <acrameinstein@gmail.com>
2023-09-10 08:37:07 +0300

    add static files

--- routes/Admin.js
+++ routes/Admin.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 router.get('/add-product', (req, res,next) => {
-    res.send('<form method="POST" action="/product"><input type="text" name="title" ><button type="subit">AddProduct</button></form>');
+    res.sendFile(path.join(__dirname, '../','views','add-product.html'));
 });
 
 router.post('/product', (req, res,next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 router.get('/m',(req, res,next) => {
     console.log('Running');
-    res.send('This is the introduction to express');
+    res.sendFile(path.join(__dirname, '../','views','shop.html'));
     
 });
  module.exports = router
