a7eeaeccdfc2cbc696a8a1315cdad4dc7693ad64
engpaulmutebi <engpaulmutebi@gmail.com>
2023-09-20 14:03:19 +0300

    defining the product

--- models/product.js
+++ models/product.js
@@ -1,28 +1,24 @@
-const db = require('../util/database');
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
 
-const Cart = require('./cart');
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-
-  save() {
-    return db.execute(
-      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static deleteById(id) {}
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {}
-};
+const Product = sequelize.define('Product',{
+  id:{ 
+    type:sequelize.INTERGER,
+    autoIncrement:true,
+    allowNull:false,
+    primaryKey:true,
+  },
+  title:sequelize.STRING,
+  price:{
+    type:sequelize.DOUBLE,
+    allowNull:false,
+  },
+  imageUlr:{
+    type:sequelize.STRING,
+    allowNull:false,
+  },
+  description:{
+    type:sequelize.STRING,
+    allowNull:false,
+  }});
+module.exports = Product;
