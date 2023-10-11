5b0e11e992e7fee93f4a99d41835ebdb82d8e833
engpaulmutebi <engpaulmutebi@gmail.com>
2023-10-11 08:19:28 +0300

    adding product schema using mongoose

--- models/product.js
+++ models/product.js
@@ -1,3 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    title:{
+        type: 'string',
+        required: true
+    },
+    price:{
+        type: 'Number',
+        required: true
+    },
+    description:{
+        type: 'string',
+        required: true
+    },
+    imageUrl:{
+        type: 'string',
+        required: true
+    }
+});
 // const Sequelize = require('sequelize');
 
 // const sequelize = require('../util/database');
